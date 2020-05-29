1bcc6137ac2540dbd2e1e1f6001261a7b27477dd
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-05-29 07:59:09 +0300

    Deleted unnecessary files

--- env/libs/polyfill/all.js
+++ env/libs/polyfill/all.js
@@ -1,6 +1,1 @@
 import './polyfills/custom'
-// import './polyfills/url.min'
-// import './polyfills/promise.min'
-import './polyfills/fetch.iife'
-import 'abortcontroller-polyfill/src/abortcontroller-polyfill'
-import './polyfills/dom'
--- env/libs/polyfill/polyfills/dom.js
+++
@@ -1,9 +0,0 @@
-if ('keyIdentifier' in KeyboardEvent.prototype) {
-	Object.defineProperty(KeyboardEvent.prototype, 'code', {
-		enumerable  : true,
-		configurable: true,
-		get() {
-			return this.keyIdentifier
-		}
-	})
-}
