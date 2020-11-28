acb115f64028360f8a9b3f87754b45fd9b4236e2
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-11-28 18:56:05 +0200

    Add assets folders to dist

--- env/scripts/specific/builds.js
+++ env/scripts/specific/builds.js
@@ -4,12 +4,12 @@ const {reCreateDir, deletePaths} = require('../../common/helpers')
 
 const buildMjs = singleCall(async appConfigType => {
 	await reCreateDir(`dist/${appConfigType}/mjs`)
-	await run(`cpy "**/*.(css|html|svelte)" "../dist/${appConfigType}/mjs/" --parents --cwd=src`)
+	await run(`cpy "**/*.(css|html|svelte)" "**/assets/*" "../dist/${appConfigType}/mjs/" --parents --cwd=src`)
 	await run(`babel src -x .js -x .ts --out-dir dist/${appConfigType}/mjs/ --no-babelrc --config-file ./env/babel/configs/dist-mjs.js`)
 })
 const buildJs = singleCall(async appConfigType => {
 	await reCreateDir(`dist/${appConfigType}/js`)
-	await run(`cpy "**/*.(css|html|svelte)" "../dist/${appConfigType}/js/" --parents --cwd=src`)
+	await run(`cpy "**/*.(css|html|svelte)" "**/assets/*" "../dist/${appConfigType}/js/" --parents --cwd=src`)
 	await run(`babel src -x .js -x .ts --out-dir dist/${appConfigType}/js/ --no-babelrc --config-file ./env/babel/configs/dist-js.js`)
 })
 const buildComponents = singleCall(async appConfigType => {
