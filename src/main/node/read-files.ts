bd51f6fd398075cf763f62957eb158581ffecfb3
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2021-04-18 17:09:18 +0300

    Develop

--- src/main/node/read-files.ts
+++ src/main/node/read-files.ts
@@ -1,19 +1,15 @@
 import fse from 'fs-extra'
 
-export function forEachFiles({
+export function processFiles({
 	fileOrDirPath,
 	excludePaths,
 	processFile,
-	parallelCount = 1,
 }: {
 	fileOrDirPath: string,
 	excludePaths: (fileOrDirPath: string) => boolean,
 	processFile: (fileName: string) => Promise<void>,
-	parallelCount: number,
 }) {
-	const promises: Promise<void>[] = []
-
-	async function _forEachFiles(fileOrDirPath: string) {
+	async function _processFiles(fileOrDirPath: string) {
 		if (excludePaths(fileOrDirPath)) {
 			return
 		}
@@ -22,24 +18,16 @@ export function forEachFiles({
 		if (!stat.isDirectory()) {
 			const promise = processFile(fileOrDirPath)
 			if (promise) {
-				promises.push(promise)
-				promise.finally(() => {
-					const index = promises.indexOf(promise)
-					promises[index] = promises[promises.length - 1]
-					promises.length--
-				})
-				while (promises.length >= parallelCount) {
-					await Promise.race(promises)
-				}
+				await promise
 			}
 			return
 		}
 
 		const subPaths = await fse.readdir(fileOrDirPath)
 		for (let i = 0, len = subPaths.length; i < len; i++) {
-			_forEachFiles(subPaths[i])
+			_processFiles(subPaths[i])
 		}
 	}
 
-	return _forEachFiles(fileOrDirPath)
+	return _processFiles(fileOrDirPath)
 }
